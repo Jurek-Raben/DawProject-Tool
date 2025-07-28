105651fccb3445311aed194b1913d02372c18cf1
Jurek Raben <jurekraben@googlemail.com>
2025-07-29 00:32:01 +0200

    v0.1.1: Add clip mute, fix sequence mute

--- build_for_mac_linux.sh
+++ build_for_mac_linux.sh
@@ -1,2 +1,2 @@
 cd ./ffx.tools.DawProject.xrnx
-tar -acvf ../build/ffx.tools.DawProject.xrnx --exclude="./tmp" --exclude="/.git" --exclude="/.vscode" --exclude="./examples" --exclude="./tmp" --exclude="./config.json" *
+tar -acvf ../build/ffx.tools.DawProject.xrnx --exclude="./tmp" --exclude="/.github" --exclude="/.git" --exclude="/.vscode" --exclude="./examples" --exclude="./tmp" --exclude="./config.json" *
