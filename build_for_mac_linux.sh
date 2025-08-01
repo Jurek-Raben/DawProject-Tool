8062708b88de2e20c878df58ceaef174fd759d20
Jurek Raben <jurekraben@googlemail.com>
2025-08-01 21:23:35 +0200

    add vst2info workaround tool, add cc and pb automation, fix automation, add fxp preset generation

--- build_for_mac_linux.sh
+++ build_for_mac_linux.sh
@@ -2,6 +2,6 @@
 # pack renoise tool
 cd ./ffx.tools.DawProject.xrnx
 rm ../build/ffx.tools.DawProject.xrnx
-#zip -r ../build/ffx.tools.DawProject.xrnx . -x "./tmp/*" "/.github/*" "/.git/*" "/.vscode/*" "./examples/*" "./tmp/*" "./config.json" ".DS_Store" ".gitignore"
-tar -acvf ../build/ffx.tools.DawProject.zip --exclude="./tmp" --exclude="/.github" --exclude="/.git" --exclude="/.vscode" --exclude="./examples" --exclude="./tmp" --exclude="./config.json" *
-mv ../build/ffx.tools.DawProject.zip ../build/ffx.tools.DawProject.xrnx
+zip -r ../build/ffx.tools.DawProject.xrnx . -x "./tmp/*" "/.github/*" "/.git/*" "/.vscode/*" "./examples/*" "./tmp/*" "./config.json" ".DS_Store" ".gitignore"
+#tar -acvf ../build/ffx.tools.DawProject.zip --exclude="./tmp" --exclude="/.github" --exclude="/.git" --exclude="/.vscode" #--exclude="./examples" --exclude="./tmp" --exclude="./config.json" *
+#mv ../build/ffx.tools.DawProject.zip ../build/ffx.tools.DawProject.xrnx
