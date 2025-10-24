29ad412a7914d064a4d1caaf2ca9d9d1dc1473fc
Jurek Raben <jurekraben@googlemail.com>
2025-10-24 22:24:14 +0200

    bump version to 0.1.10

--- ffx.tools.DawProject.xrnx/main.lua
+++ ffx.tools.DawProject.xrnx/main.lua
@@ -3,7 +3,7 @@ end
 --------------------------------------------------------------------------------
 -- Daw Project
 -- by Jurek Raben
--- v0.1.9
+-- v0.1.10
 --
 -- Licensed under CC Attribution-NonCommercial-ShareAlike 4.0 International
 -- Info here: https://creativecommons.org/licenses/by-nc-sa/4.0/
